23762824e109d0956987195bb4b3d4e5e527b734
martin.hajdu <jmhajdu@gmail.com>
2023-04-05 09:42:40 +0200

    Gateway

--- src/Controllers/ProcessController.php
+++ src/Controllers/ProcessController.php
@@ -2,12 +2,23 @@
 
 namespace Mhajdu\PayitGateway\Controllers;
 
+use Mhajdu\PayitGateway\Traits\Helpers;
+
 class ProcessController {
+
+    use Helpers;
+
     protected $request;
     public function __construct($request) {
         $this->request = $request;
+        $this->requestData = $this->decryptData($request->data);
     }
+
+    public function failed() {
+        return $this->requestData['status'] != 'success';
+    }
+
     public function successful() {
-        return $this->request->successful();
+        return $this->requestData['status'] == 'success';
     }
 }
